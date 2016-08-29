6e61271c9c904217fe65ecf710781c3d184a20ed
Hanawa Takuro <takuro@Macintosh.local>
2016-08-30 00:58:12 +0900

    initial

---
+++ Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Hanawa Takuro on 2016/08/30.
+//  Copyright © 2016年 Hanawa Takuro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
